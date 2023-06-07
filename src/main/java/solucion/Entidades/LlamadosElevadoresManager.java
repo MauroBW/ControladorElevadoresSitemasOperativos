0851eb3097ab5baf92c48fe3dd3330c42b03d0f7
MauroBW <maurobotti99@gmail.com>
2023-06-07 19:32:30 -0300

    updates en lista de pasajeros actuales

--- src/main/java/solucion/Entidades/LlamadosElevadoresManager.java
+++ src/main/java/solucion/Entidades/LlamadosElevadoresManager.java
@@ -1,35 +1,47 @@
 package solucion.Entidades;
 
+import org.springframework.boot.actuate.endpoint.web.Link;
+
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.Semaphore;
 
 public class LlamadosElevadoresManager {
     private static List<Pasajero> listaPasajeros = new LinkedList<>();
+    private static List<Pasajero> storage = new LinkedList<>();
 
     public LlamadosElevadoresManager() {
     }
 
     public static void agregarPasajero(Pasajero pasajero) {
         pasajero.start();
-        listaPasajeros.add(pasajero);
+        storage.add(pasajero);
     }
 
-    public static void agregarPasajero(String nombre, int peso, int pisoOrigen, int pisoDestino) {
-        Pasajero pasajero = new Pasajero(nombre, peso, pisoOrigen, pisoDestino);
+    public static void agregarPasajero(String nombre, int peso, int pisoOrigen, int pisoDestino, int tiempoInicio) {
+        Pasajero pasajero = new Pasajero(nombre, peso, pisoOrigen, pisoDestino, tiempoInicio);
         /*
 
             Acá se va a generar la lógica al agregar el pasajero.
             A su vez esta clase debe controlar o saber el tiempo, ya que a cada cambio de tiempo se debe reordenar la lista.
 
          */
-        listaPasajeros.add(pasajero);
+        storage.add(pasajero);
+    }
+
+    public static void updateListaPedidos(int tiempoUpdate) {
+        List<Pasajero> pasajerosAsignados = new LinkedList<>();
+        for (Pasajero pasajero : storage) {
+            if(pasajero.getTiempoInicio() == tiempoUpdate) {
+                pasajerosAsignados.add(pasajero);
+                listaPasajeros.add(pasajero);
+            }
+        }
+        storage.removeAll(pasajerosAsignados);
     }
 
     public static void iniciarElevadores() {
+        updateListaPedidos(0);
         for (Pasajero pasajero : listaPasajeros) {
             pasajero.start();
         }
