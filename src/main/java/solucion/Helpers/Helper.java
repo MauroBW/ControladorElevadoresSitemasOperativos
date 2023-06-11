71afce38abc0382edf0f870403181803de3604a3
MauroBW <33839339+MauroBW@users.noreply.github.com>
2023-06-11 00:16:53 -0300

    dependencia para leer csvs de una + helper para leer csv y crear pasajeros

--- src/main/java/solucion/Helpers/Helper.java
+++ src/main/java/solucion/Helpers/Helper.java
@@ -1,7 +1,17 @@
 package solucion.Helpers;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import solucion.Entidades.LlamadosElevadoresManager;
+import solucion.Entidades.Pasajero;
+
+import java.io.FileReader;
+import java.io.Reader;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
 public class Helper {
@@ -23,5 +33,38 @@ public class Helper {
         }
 
         return sb.toString();
+    }
+
+    public static List<Pasajero> leerSimulacion(String filepath) {
+        List<Pasajero> pasajeros = new ArrayList<>();
+
+        try {
+            Reader in = new FileReader(filepath);
+            CSVParser parser = CSVFormat.DEFAULT
+                    .withHeader("nombre", "peso", "pisoActual", "pisoObjetivo", "tiempoInicio")
+                    .withDelimiter(',')
+                    .parse(in);
+
+            for (CSVRecord record : parser) {
+                String nombre = record.get("nombre");
+                int peso = Integer.parseInt(record.get("peso"));
+                int pisoActual = Integer.parseInt(record.get("pisoActual"));
+                int pisoObjetivo = Integer.parseInt(record.get("pisoObjetivo"));
+                int tiempoInicio = Integer.parseInt(record.get("tiempoInicio"));
+
+                Pasajero pasajero = new Pasajero(nombre, peso, pisoActual, pisoObjetivo, tiempoInicio);
+                pasajeros.add(pasajero);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return pasajeros;
+    }
+
+    public static void cargarSimulacion(List<Pasajero> pasajeros) {
+        for (Pasajero pasajero : pasajeros) {
+            LlamadosElevadoresManager.agregarPasajero(pasajero);
+        }
     }
 }
