25d71db37e4c575483f91eef50697009b129a8fa
MauroBW <33839339+MauroBW@users.noreply.github.com>
2023-06-16 01:13:30 -0300

    Print con colores en terminal

--- src/main/java/solucion/Helpers/Helper.java
+++ src/main/java/solucion/Helpers/Helper.java
@@ -67,4 +67,30 @@ public class Helper {
             LlamadosElevadoresManager.agregarPasajero(pasajero);
         }
     }
+
+
+    public enum ConsoleColor {
+        RESET("\u001B[0m"),
+        BLACK("\u001B[30m"),
+        RED("\u001B[31m"),
+        GREEN("\u001B[32m"),
+        YELLOW("\u001B[33m"),
+        BLUE("\u001B[34m"),
+        PURPLE("\u001B[35m"),
+        CYAN("\u001B[36m"),
+        WHITE("\u001B[37m");
+
+        private final String code;
+
+        ConsoleColor(String code) {
+            this.code = code;
+        }
+
+        public String getCode() {
+            return code;
+        }
+    }
+    public static void colorizer(ConsoleColor color, String text) {
+        System.out.println(color.getCode() + text + ConsoleColor.RESET.getCode());
+    }
 }
